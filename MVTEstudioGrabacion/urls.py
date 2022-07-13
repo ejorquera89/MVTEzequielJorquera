ff4d35bdf5096090500a08ca4a3260b287f96304
ejorquera89 <ejorquera16@gmail.com>
2022-07-12 21:31:59 -0300

    CRUD Clases basadas en vistas

--- MVTEstudioGrabacion/urls.py
+++ MVTEstudioGrabacion/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from MVTEstudioGrabacion.views import inicio, leerMusico, musicos, cantantes, empleados, discos, contacto, musicosForm, cantanteForm, empleadoForm, discoForm, busquedaMusico, buscar, leerMusico
+from MVTEstudioGrabacion.views import MusicoCreate, MusicoDetail, MusicoEdit, MusicoEliminar, MusicoList, editarCantante, editarDisco, editarEmpleado, editarMusico, eliminarCantante, eliminarDisco, eliminarEmpleado, eliminarMusico, inicio, musicos, leerCantante, leerEmpleado, leerDisco, contacto, musicosForm, cantanteForm, empleadoForm, discoForm, busquedaMusico, buscar, leerMusico, about
 
 urlpatterns = [
     path('', inicio, name = 'inicio'),
     path('musicos/', leerMusico, name = 'musicos'),
-    path('cantantes/', cantantes, name = 'cantantes'),
-    path('empleados/', empleados, name = 'empleados'),
-    path('discos/', discos, name = 'discos'),
+    path('cantantes/', leerCantante, name = 'cantantes'),
+    path('empleados/', leerEmpleado, name = 'empleados'),
+    path('discos/', leerDisco, name = 'discos'),
     path('contacto/', contacto, name = 'contacto'),
     path('musicosForm/', musicosForm, name = 'musicosForm'),
     path('cantanteForm/', cantanteForm, name = 'cantantesForm'),
@@ -14,4 +14,19 @@ urlpatterns = [
     path('discoForm/', discoForm, name = 'discoForm'),
     path('busquedaMusico/', busquedaMusico, name = 'busquedaMusico'),
     path('buscar/', buscar, name = 'buscar'),
+    path('about/', about, name='about'),
+    path('eliminarMusico/<nombre>', eliminarMusico, name='eliminarMusico'),
+    path('eliminarCantante/<nombre>', eliminarCantante, name='eliminarCantante'),
+    path('eliminarEmpleado/<nombre>', eliminarEmpleado, name='eliminarEmpleado'),
+    path('eliminarDisco/<nombre>', eliminarDisco, name='eliminarDisco'),
+    path('editarMusico/<nombre>', editarMusico, name='editarMusico'),
+    path('editarCantante/<nombre>', editarCantante, name='editarCantante'),
+    path('editarDisco/<nombre>', editarDisco, name='editarDisco'),
+    path('editarEmpleado/<nombre>', editarEmpleado, name='editarEmpleado'),
+
+    path('musico/list/', MusicoList.as_view(), name='musico_list'),
+    path('musico/<pk>/', MusicoDetail.as_view(), name='musico_detalle'),
+    path('musico/nuevo/', MusicoCreate.as_view(), name='musico_crear'),
+    path('musico/edicion/<pk>', MusicoEdit.as_view(), name='musico_editar'),
+    path('musico/borrar/<pk>', MusicoEliminar.as_view(), name='musico_eliminar'),
 ]
--- MVTEstudioGrabacion/views.py
+++ MVTEstudioGrabacion/views.py
@@ -1,13 +1,15 @@
 from multiprocessing import context
+from pickle import GET
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from MVTEstudioGrabacion.models import Musico, Cantante, Empleado, Disco
 from django.template import loader
 from MVTEstudioGrabacion.forms import MusicoForm
 from MVTEstudioGrabacion.forms import CantanteForm
 from MVTEstudioGrabacion.forms import EmpleadoForm
 from MVTEstudioGrabacion.forms import DiscoForm
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 
 def inicio(self):
@@ -116,3 +118,170 @@ def leerMusico(request):
     musicos = Musico.objects.all()
     contexto = {'musicos': musicos}
     return render (request, 'MVTEstudioGrabacion/musicos.html', contexto)
+
+def leerCantante(request):
+    cantantes = Cantante.objects.all()
+    contexto = {'cantantes': cantantes}
+    return render (request, 'MVTEstudioGrabacion/cantantes.html', contexto)
+
+def leerEmpleado(request):
+    empleados = Empleado.objects.all()
+    contexto = {'empleados': empleados}
+    return render (request, 'MVTEstudioGrabacion/empleados.html', contexto)
+
+def leerDisco(request):
+    discos = Disco.objects.all()
+    contexto = {'discos': discos}
+    return render (request, 'MVTEstudioGrabacion/discos.html', contexto)
+
+def about(request):
+    return render (request, 'MVTEstudioGrabacion/about.html')
+
+#Views de eliminación
+
+def eliminarMusico(request, nombre):
+    musico= Musico.objects.get(nombre=nombre)
+    musico.delete()
+
+    musico=Musico.objects.all()
+    contexto= {'musicos':musico}
+    return render (request, 'MVTEstudioGrabacion/musicos.html', contexto)
+
+def eliminarCantante(request, nombre):
+    cantante= Cantante.objects.get(nombre=nombre)
+    cantante.delete()
+
+    cantante=Cantante.objects.all()
+    contexto= {'cantantes':cantante}
+    return render (request, 'MVTEstudioGrabacion/cantantes.html', contexto)
+
+def eliminarEmpleado(request, nombre):
+    empleados= Empleado.objects.get(nombre=nombre)
+    empleados.delete()
+
+    empleados=Empleado.objects.all()
+    contexto= {'empleados':empleados}
+    return render (request, 'MVTEstudioGrabacion/empleados.html', contexto)
+
+def eliminarDisco(request, nombre):
+    discos = Disco.objects.get(nombre=nombre)
+    discos.delete()
+
+    discos=Disco.objects.all()
+    contexto= {'discos':discos}
+    return render (request, 'MVTEstudioGrabacion/discos.html', contexto)
+
+#Views de Edición
+
+def editarMusico(request, nombre):
+    musico=Musico.objects.get(nombre=nombre)
+    if request.method == 'POST':
+        miFormulario = MusicoForm(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            musico.nombre = informacion['nombre']
+            musico.apellido = informacion['apellido']
+            musico.email = informacion['email']
+            musico.instrumento = informacion['instrumento']
+            musico.save()
+
+            musicos = Musico.objects.all()
+            contexto = {'musicos':musicos}
+            return render (request, 'MVTEstudioGrabacion/musicos.html', contexto)
+    else:
+        miFormulario = MusicoForm(initial= {'nombre':musico.nombre, 'apellido': musico.apellido, 'email':musico.email, 'instrumento':musico.instrumento})
+        contexto = {'miFormulario': miFormulario, 'nombre':nombre}
+        return render(request, 'MVTEstudioGrabacion/editarMusico.html', contexto)
+
+def editarCantante(request, nombre):
+    cantante=Cantante.objects.get(nombre=nombre)
+    if request.method == 'POST':
+        miFormulario = CantanteForm(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            cantante.nombre = informacion['nombre']
+            cantante.apellido = informacion['apellido']
+            cantante.email = informacion['email']
+            cantante.tipodevoz = informacion['tipodevoz']
+            cantante.save()
+
+            cantantes = Cantante.objects.all()
+            contexto = {'cantantes':cantantes}
+            return render (request, 'MVTEstudioGrabacion/cantantes.html', contexto)
+    else:
+        miFormulario = CantanteForm(initial= {'nombre':cantante.nombre, 'apellido': cantante.apellido, 'email':cantante.email, 'tipodevoz':cantante.tipodevoz})
+        contexto = {'miFormulario': miFormulario, 'nombre':nombre}
+        return render(request, 'MVTEstudioGrabacion/editarCantante.html', contexto)
+
+def editarEmpleado(request, nombre):
+    empleado=Empleado.objects.get(nombre=nombre)
+    if request.method == 'POST':
+        miFormulario = EmpleadoForm(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            empleado.nombre = informacion['nombre']
+            empleado.apellido = informacion['apellido']
+            empleado.email = informacion['email']
+            empleado.profesion = informacion['profesion']
+            empleado.save()
+
+            empleados = Empleado.objects.all()
+            contexto = {'empleados':empleados}
+            return render (request, 'MVTEstudioGrabacion/empleados.html', contexto)
+    else:
+        miFormulario = EmpleadoForm(initial= {'nombre':empleado.nombre, 'apellido': empleado.apellido, 'email':empleado.email, 'profesion':empleado.tipodevoz})
+        contexto = {'miFormulario': miFormulario, 'nombre':nombre}
+        return render(request, 'MVTEstudioGrabacion/editarEmpleado.html', contexto)
+
+
+def editarDisco(request, nombre):
+    disco=Disco.objects.get(nombre=nombre)
+    if request.method == 'POST':
+        miFormulario = DiscoForm(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            disco.nombre = informacion['nombre']
+            disco.fechaDeEstreno = informacion['fechaDeEstreno']
+            disco.Productora = informacion['productora']
+            disco.save()
+
+            discos = Disco.objects.all()
+            contexto = {'discos':discos}
+            return render (request, 'MVTEstudioGrabacion/discos.html', contexto)
+    else:
+        miFormulario = DiscoForm(initial= {'nombre':disco.nombre, 'fechaDeEstreno': disco.fechaDeEstreno, 'Productora':disco.Productora})
+        contexto = {'miFormulario': miFormulario, 'nombre':nombre}
+        return render(request, 'MVTEstudioGrabacion/editarDisco.html', contexto)
+
+#Clases basadas en Vistas
+
+#LISTVIEW
+class MusicoList(ListView):
+    modelo = Musico
+    template_name = 'MVTEstudioGrbacion/musicos.html'
+
+#DETAILVIEW
+
+class MusicoDetail(DetailView):
+    modelo = Musico
+    template_name = 'MVTEstudioGrabacion/musicoDetalle.html'
+
+#CREATEVIEW
+
+class MusicoCreate(CreateView):
+    modelo = Musico
+    success_url = reverse_lazy('musicos')
+    fields = ['nombre', 'apellido', 'email', 'instrumento']
+
+#EDITVIEW
+
+class MusicoEdit(UpdateView):
+    modelo = Musico
+    success_url = reverse_lazy('musicos')
+    fields = ['nombre', 'apellido', 'email', 'instrumento']
+
+#DELETEVIEW
+
+class MusicoEliminar(DeleteView):
+    modelo = Musico
+    success_url = reverse_lazy('musicos')
